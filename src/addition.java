9db7f12e589f864599884b1f9daef9b897e65b9b
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-21 22:11:06 +0530

    addition

--- src/addition.java
+++ src/addition.java
@@ -2,6 +2,6 @@ public class addition {
     public static void main(String[] args) {
         int num1=5,num2=15,sum;
         sum=num1+num2;
-        System.out.println("sum of two numbers:"+sum);
+        System.out.println(sum);
     }
 }
